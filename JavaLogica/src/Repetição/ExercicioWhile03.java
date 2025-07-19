541d85c343b9187ae47a4a24137e54f493540720
IagoPelitero <p.iago.ip@gmail.com>
2025-07-18 21:48:12 -0300

    Docs: corrgido o exercicio 03

--- JavaLogica/src/Repetição/ExercicioWhile03.java
+++ JavaLogica/src/Repetição/ExercicioWhile03.java
@@ -6,28 +6,24 @@ public class ExercicioWhile03 {
 
 	public static void main(String[] args) {
 		Scanner ler = new Scanner(System.in);
-		int proximo=0,anterior=0,atual=1,limite=0, resp=-1;
-		
-		while(resp==-1) {
-			
-			System.out.println("Digite um número para gerar uma sequência em Fibonacci");
+		int resp=-1;
+		while(resp==-1) 
+		{
+			int proximo=0,anterior=0,atual=1,limite;
+			System.out.println("Quantos numeros fibonnacci deseja gerar? ");
 			limite=ler.nextInt();
-				while(proximo<limite) {
-				System.out.println(atual);
-				
+			System.out.println(atual);
+			for(int n=1;n<limite;n++)
+			{
 				proximo=atual+anterior;
 				System.out.println(proximo);
 				anterior=atual;
 				atual=proximo;
-				}
-				break;
 			}
-						
-		    System.out.println("Digite 1 se deseja gerar outra sequência: ");
-			limite=ler.nextInt();		
-			
-			}
-		
+			System.out.println("Pressione -1 para repetir: ");
+		}
 	}
+}
+
 		
 	
