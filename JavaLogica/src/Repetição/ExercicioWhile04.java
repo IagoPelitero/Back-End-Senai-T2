f65dbd46d436f3e8129fc8980b32dea95dca6606
Iago Pelitero <160602490+IagoPelitero@users.noreply.github.com>
2025-07-20 13:04:33 -0300

    Feat: concluido tabuada com while

--- JavaLogica/src/Repetição/ExercicioWhile04.java
+++ JavaLogica/src/Repetição/ExercicioWhile04.java
@@ -6,8 +6,18 @@ public class ExercicioWhile04 {
 
 	public static void main(String[] args) {
 		Scanner ler = new Scanner(System.in);
-		
-
+		int resp=-1;
+		while(resp=-1){
+			int n, i;
+			System.out.printIn("Digite um número para mostrar a tabuada: ");
+			for(n=ler.nextInt();n<=9;n++)
+		{
+			System.out.println("Tabuado do "+n);
+			for(i=1;i<=10;i++)
+				System.out.println(n+" x "+i+" = "+n*i);
+		}
+			System.out.printIn("Se deseja criar começar por outro digite -1: ");
+			n=ler.nextIn();
 	}
 
 }
