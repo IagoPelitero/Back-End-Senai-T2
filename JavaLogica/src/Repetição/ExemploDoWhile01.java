d4ccb77bee0ebdf6d7bec58944b3ed21f31d2e5e
IagoPelitero <p.iago.ip@gmail.com>
2025-07-21 18:50:16 -0300

    docs: incluído arquivo e exemplo do-while

---
+++ JavaLogica/src/Repetição/ExemploDoWhile01.java
@@ -0,0 +1,21 @@
+package Repetição;
+
+import java.util.Scanner;
+
+public class ExemploDoWhile01 {
+
+	public static void main(String[] args) {
+		Scanner ler = new Scanner (System.in);
+		int nr;
+		do{
+			System.out.print("Digite um numero (0 para finalizar):");
+			nr=ler.nextInt();
+			if (nr%2==0) //se o resto da divisão por 2 for 0
+				System.out.print("Este número é par");
+			else
+				System.out.print("Este número é impar");
+		}while (nr!=0); //enquanto nr for diferente de 0
+
+	}
+
+}
