100cee3de5aaffb8560fc9a7c390727f5e5d82e4
IagoPelitero <p.iago.ip@gmail.com>
2025-07-22 19:41:53 -0300

    test: incluído exercicios de vetores

---
+++ JavaLogica/src/vetores/ExercicioVetor04.java
@@ -0,0 +1,26 @@
+package vetores;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ExercicioVetor04 {
+
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in); 
+		int vet1[] = new int[10];
+		int vet2[] = new int[10];
+		int soma[] = new int[10];
+		System.out.println("Digite a primeira sequência de números: ");
+		for(int n1 = 0; n1<10; n1++) {
+			vet1[n1]=ler.nextInt();
+		}
+		System.out.println("Digite a segunda sequência de números: ");
+		for(int n2 = 0; n2<10; n2++)
+			vet2[n2]=ler.nextInt();
+	
+		for(int n3=0; n3<10; n3++)
+			soma[n3]= vet1[n3]+vet2[n3];
+		
+		System.out.println("A soma dos números são: "+Arrays.toString(soma));
+}
+	}
---
+++ aula09.md
@@ -0,0 +1,6 @@
+# Continuação de Vetores
+
+4 - Faça um programa com 3 vetores de 10 elementos cada onde cada usuário alimente os 2 primeiros
+vetores com números inteiros e o 3º vetor seja alimentado com a soma dos elementos dos vetores anteriores. No final imprima os elementos dos 3 vetores.
+
+5 - Faça um programa com três vetores de 10 elementos cada, onde o primeiro será alimentado com os 10 primeiros elementos da sequência de fibonacci. Para o segundo vetor o usuário irá informar o valor de inicio e a razão para gerar uma PA que irá alimenta-lo. O terceiro vetor será alimentado com a PG, onde o usuário ira informar o valor de inicio e a razão. No final imprima os elementos dos três vetores.
