81bafad696241193837c238c1c0a996fcf609aab
IagoPelitero <p.iago.ip@gmail.com>
2025-07-23 21:45:15 -0300

    test: adicionado exercicio Matriz

--- JavaLogica/src/Matriz/ExemploMatriz01.java
+++ JavaLogica/src/Matriz/ExemploMatriz01.java
@@ -3,6 +3,7 @@ package Matriz;
 import java.util.Scanner;
 
 public class ExemploMatriz01 {
+	public static void main(String[] args) {
 	int[][] mat = new int[3][3];
 	int soma=0,l,c;
 	Scanner ler = new Scanner(System.in);
@@ -10,10 +11,21 @@ public class ExemploMatriz01 {
 	{
 		for(c=0;c<3;c++)
 		{
-			System.out.println("Diite o valor para a linha"+l+"coluna"+c);
+			System.out.println("Digite o valor para a linha "+l+" coluna "+c);
 			mat[l][c]=ler.nextInt();
 			soma+=mat[l][c];
-			soma=soma+mat[l][c];
+			//soma=soma+mat[l][c]; outro método de soma
 		}
 	}
+		for(l=0;l<3;l++)
+		{
+			for(c=0;c<3;c++)
+			{
+				System.out.println(mat[l][c]+"\t");
+			}
+			System.out.println();
+		}
+		System.out.println("A soma dos valores da matriz é: "+soma);
+	}
 }
+
---
+++ JavaLogica/src/Matriz/ExercicioMatriz01.java
@@ -0,0 +1,31 @@
+package Matriz;
+
+import java.util.Scanner;
+
+public class ExercicioMatriz01 {
+
+	public static void main(String[] args) {
+			int[][] mat = new int[3][4];
+			int mult=2,l,c;
+			Scanner ler = new Scanner(System.in);
+			for(l=0;l<3;l++)
+			{
+				for(c=0;c<4;c++)
+				{
+					System.out.println("Digite o valor para a linha "+l+" coluna "+c);
+					mat[l][c]=ler.nextInt();
+					mult*=mat[l][c];
+					//soma=soma+mat[l][c]; outro método de soma
+				}
+			}
+				for(l=0;l<3;l++)
+				{
+					for(c=0;c<4;c++)
+					{
+						System.out.println(mat[l][c]+"\t");
+					}
+					System.out.println();
+				}
+				System.out.println("A multiplicação dos valores da matriz é: "+mult);
+			}
+		}
