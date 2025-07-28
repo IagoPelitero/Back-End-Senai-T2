42295aecd8c4c0b426ecbdb222544162e8e2527b
IagoPelitero <p.iago.ip@gmail.com>
2025-07-28 19:19:28 -0300

    Docs: adicionado aula 13 e package excecao

---
+++ JavaLogica/src/Excecao/ExemploTry01.java
@@ -0,0 +1,23 @@
+package Excecao;
+
+import java.util.Scanner;
+
+public class ExemploTry01 {
+
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		int n1, n2, resp;
+		System.out.println("Digite o primeiro número: ");
+		n1=ler.nextInt();
+		System.out.println("Digite o segundo número: ");
+		n2=ler.nextInt();
+		try {
+			resp=n1/n2;
+			System.out.println("A divisao é: "+resp);
+		}catch(ArithmeticException erro) {
+			System.out.println("Erro: Divisão por zero");
+		}
+
+	}
+
+}
