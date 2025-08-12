01ca88edcc1e3ad928eccfff9d5b8163e55da3ab
IagoPelitero <p.iago.ip@gmail.com>
2025-08-11 21:30:15 -0300

    fix: corrigido e agregado mais dados de heranca

--- JavaPOO/src/Heranca/ExemploHeranca01.java
+++ JavaPOO/src/Heranca/ExemploHeranca01.java
@@ -4,7 +4,12 @@ public class ExemploHeranca01 {
 
 	public static void main(String[] args) {
 		Aluno a = new Aluno();
+		Pessoa p = new Pessoa();
 		
+		System.out.println("Dados do objeto Pessoa");
+		p.cadastro("Pedro", "Rua da visao", "11986628804", 10);
+		p.estado();
+		System.out.println("Dados do obejto Aluno");
 		a.cadastro("Iago", "Rua da visao, 0, bl82 ap21, Jd Sto Andre", "11978059550", 27);
 		a.estado();
 
