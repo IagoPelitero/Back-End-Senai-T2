f61bf6a37801e412b06e12528258cc7143ab7cdc
IagoPelitero <p.iago.ip@gmail.com>
2025-08-19 21:44:18 -0300

    test: criado arquivo do meu projeto

--- blogPessoal/blogPessoal/src/main/java/SENAI/Ipiranga/com/blogPessoal/Model/Usuario.java
+++ blogPessoal/blogPessoal/src/main/java/SENAI/Ipiranga/com/blogPessoal/Model/Usuario.java
@@ -1,9 +1,15 @@
 package SENAI.Ipiranga.com.blogPessoal.Model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -16,6 +22,10 @@ public class Usuario {
 	private String nome;
 	private String usuario;
 	private String senha;
+	
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("usuario")
+	private List<Postagem> postagem;
 	
 	public long getId() {
 		return id;
