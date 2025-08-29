4d85de5fadc404b457b1cd3f496730d1f9a979d6
IagoPelitero <p.iago.ip@gamil.com>
2025-08-28 21:05:22 -0300

    test: incluso post do blog

--- blogPessoal/blogPessoal/src/main/java/SENAI/Ipiranga/com/blogPessoal/Controller/UsuarioController.java
+++ blogPessoal/blogPessoal/src/main/java/SENAI/Ipiranga/com/blogPessoal/Controller/UsuarioController.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,5 +38,10 @@ public class UsuarioController {
 	@GetMapping("/id/{id}")
 	public ResponseEntity<Optional<Usuario>> getById(@PathVariable Long id){
 		return ResponseEntity.ok(repository.findById(id));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Usuario> post(@RequestBody Usuario usuario){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(usuario));
 	}
 }
